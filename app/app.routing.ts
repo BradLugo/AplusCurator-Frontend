55b8f07fd518f5018ded3e0133de3dcc5f9c437e
Richard Tran <redsriracha@gmail.com>
2016-09-30 06:03:38 -0500

    Formatting code

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
 
 export const routedComponents = [AppComponent];
