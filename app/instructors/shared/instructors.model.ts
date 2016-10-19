c8829482fde0b5d103ff8169dd7d945319995c65
redsriracha <redsriracha@gmail.com>
2016-10-19 18:23:14 -0500

    Created table view

--- app/instructors/shared/instructors.model.ts
+++ app/instructors/shared/instructors.model.ts
@@ -1,4 +1,16 @@
 export class Instructors {
-	id: number;
-	name: string;
+	instructorId: number;
+	firstName: string;
+	lastName: string;
+	middleName: string;
+	address: string;
+	email: string;
+	phoneNumber: string;
+	mobilePhoneNumber: string;
+	emergencyContactName: string;
+	emergencyContactPhone: string;
+	role: number;
+	status: number;
+	employmentStartDate: string;
+	employmentTerminationDate: string;
 }
